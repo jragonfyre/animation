6fa5c07bb52e7b539a1c01ff954d0a95d3f8b073
Jason Schuchardt <jargon.schuchardt@gmail.com>
2018-07-23 17:22:15 -0500

    deleted commented out lines

--- src/Geometry/Common.hs
+++ src/Geometry/Common.hs
@@ -9,24 +9,3 @@ module Geometry.Common where
 
 import Data.Array
 import Geometry.Types
-
-{-
-segmentFold :: (Segment -> a) -> (a -> b -> b) -> b -> PolyLine -> b
-segmentFold f op init (PolyLine arr) = 
-  let
-    ixs = range $ bounds arr
-    segixs = zip ixs $ tail ixs
-    segvals = map (\(ix1,ix2) -> f (arr ! ix1, arr! ix2)) segixs
-  in
-    foldr op init segvals
-
-segmentFold1 :: (Segment -> a) -> (a -> a -> a) -> PolyLine -> a
-segmentFold1 f op (PolyLine arr) = 
-  let
-    ixs = range $ bounds arr
-    segixs = zip ixs $ tail ixs
-    segvals = map (\(ix1,ix2) -> f (arr ! ix1, arr! ix2)) segixs
-  in
-    foldr1 op segvals
--}
-
