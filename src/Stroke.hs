cec10223c1dbb805c809b942c1597939af17341a
Jason Schuchardt <jargon.schuchardt@gmail.com>
2018-08-11 01:35:55 -0400

    Working on adding stroking for paths, found a bug in rasterization of regions bounded by cubic beziers, which is expected, since I haven't tested any of those yet

--- src/Stroke.hs
+++ src/Stroke.hs
@@ -143,6 +143,7 @@ buildJoin ss norm1 norm2 center =
         else -- interior join
           ([], pc)
       
+-- need to refactor the line intersection code. DRY!!!! It's really badly repeated rn
 -- takes the initial segment and the start and end of the desired new parallel segment
 -- as determined by buildJoin
 -- plus the distance 
@@ -169,7 +170,40 @@ parallelSegment d (WPathBez2 bez) s e =
           -- this *really* doesn't work near a cusp xP
   in
     [PathBez2 s (pc+.cVec)]
--- TODO implement for Bezier3. Need to be careful of inflection points and other bad shit
+parallelSegment d (WPathBez3 bez) s e =
+  let
+    ps = start3 bez
+    pc = stCont3 bez
+    pd = endCont3 bez
+    pe = end3 bez
+    norm1 = perpendicular . normalize $ pc -. ps
+    norm2 = perpendicular . normalize $ pd -. pc
+    norm3 = perpendicular . normalize $ pe -. pd
+    dVec = makeVector d d
+    mat1 = transpose $ makeMatrix norm1 norm2
+    c1 = det mat1 -- norm1 `cross` norm2 
+    inv1 = invertMatrix mat1
+    cVec1 = 
+      if c1 /= 0
+      then inv1 *. dVec
+      else
+        if norm1 == norm2
+        then d *. norm1
+        else d *. (perpendicular norm2)
+          -- this *really* doesn't work near a cusp xP again
+    mat2 = transpose $ makeMatrix norm2 norm3
+    c2 = det mat2 -- norm2 `cross` norm3 
+    inv2 = invertMatrix mat2
+    cVec2 = 
+      if c2 /= 0
+      then inv2 *. dVec
+      else
+        if norm2 == norm3
+        then d *. norm2
+        else d *. (perpendicular norm3)
+          -- this *really* doesn't work near a cusp xP still xP
+  in
+    [PathBez3 s (pc+.cVec1) (pd+.cVec2)]
 
 -- IT WORKS!!!
 strokeExterior :: StrokeStyle -> Contour -> Contour
@@ -194,7 +228,7 @@ strokeExterior ss@StrokeStyle{strokeDistance = dist} cont@Contour{contourSegs=cs
     makeContour strokecs
 
 strokeTestS :: StrokeStyle
-strokeTestS = StrokeStyle 10 (BezierJoin 0) (squareCap)
+strokeTestS = StrokeStyle 1 (BezierJoin 0) (squareCap)
 
 strokeTestC :: Contour
 strokeTestC = makeContour 
@@ -202,6 +236,11 @@ strokeTestC = makeContour
   , PathBez2 (makePoint 110 10) (makePoint 60 110)
   ]
 
+strokeTestC3 :: Contour
+strokeTestC3 = makeContour
+  [ PathSeg (makePoint 10 10)
+  , PathBez3 (makePoint 110 10) (makePoint 10 110) (makePoint 110 110)
+  ]
 
 
 -- stroke returns the inner and exterior contours (in that order)
@@ -210,7 +249,40 @@ strokeTestC = makeContour
 strokeContour :: StrokeStyle -> Contour -> (Contour, Contour)
 strokeContour s c = (strokeExterior s $ reverseC c, strokeExterior s c)
 
+-- easy modification of strokeExterior
+-- This all needs refactoring so I DRM xP don't repeat myself
+-- but do the modification first!
+-- think I need to somehow unify paths with contours.
+-- they behave basically the same for a lot of applications
+strokePathExterior :: StrokeStyle -> Path -> Path
+strokePathExterior ss@StrokeStyle{strokeDistance = dist} path@Path{pathSegs=(ps,cap)} = 
+  let
+    --csegL = V.toList cs
+    ws = toWholeSegsP path
+    n = V.length ps
+    begCap = buildCap ss (
+    endCap = 
+    starts = fmap pSegStart cs
+    ends = V.imap (\i _ -> pSegStart $ (cs!((i+1)`mod`n))) cs
+    normal1s = V.map (\wps -> pathNormal wps 1) ws
+    normal2s = V.imap (\i _ -> pathNormal (ws!((i+1)`mod`n)) 0) ws
+    joins = V.zipWith3 (buildJoin ss) normal1s normal2s ends
+    joinSts = V.map lPathStart joins
+    joinSegs = V.map lPathSegs joins
+    joinEnds = V.map lPathEnd joins
+    joinEndsRot = V.imap (\i _ -> joinEnds!((i-1)`mod`n)) joinEnds
+    parSegs = V.zipWith3 (parallelSegment dist) ws joinEndsRot joinSts
+    allSegs = V.zipWith (++) parSegs joinSegs
+    strokecs = concat allSegs
+  in 
+    makeContour strokecs
+
 -- strokes the path
 strokePath :: StrokeStyle -> Path -> Contour
-strokePath d p = undefined
+strokePath ss p =
+  let
+    Path{pathSegs=(ps1,_)} = strokePathExterior ss p
+    Path{pathSegs=(ps2,_)} = strokePathExterior ss $ reverseP p
+  in
+    Contour $ (V.++) ps1 ps2
 
