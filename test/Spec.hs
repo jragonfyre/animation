4cb5b5dd42e9791054d2e71609bca80c66d2dd03
Jason Schuchardt <jargon.schuchardt@gmail.com>
2018-08-31 13:30:56 -0500

    Decided to use tasty for the test suite, but the test suite is low priority

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+module Spec where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.SmallCheck as SC
+import Test.QuickCheck as QC
+
+import Geometry
+
+--theTest :: Test
+--theTest = 
+  --TestLabel
+    --"Whole Test"
+    --$ TestList 
+        --[
+        --]
+
+tests :: TestTree
+tests = testGroup "Tests" [] --[properties, unitTests]
+
+--properties :: TestTree
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
